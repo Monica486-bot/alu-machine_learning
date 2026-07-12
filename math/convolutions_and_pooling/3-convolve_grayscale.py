Fix same padding: use ceil(h/sh) output size formula

math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -9,15 +9,19 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     kh, kw = kernel.shape
     sh, sw = stride
     if padding == 'same':
-        ph = max((h - 1) * sh + kh - h, 0) // 2
-        pw = max((w - 1) * sw + kw - w, 0) // 2
+        oh = int(np.ceil(h / sh))
+        ow = int(np.ceil(w / sw))
+        ph = max((oh - 1) * sh + kh - h, 0) // 2
+        pw = max((ow - 1) * sw + kw - w, 0) // 2
     elif padding == 'valid':
         ph, pw = 0, 0
+        oh = (h - kh) // sh + 1
+        ow = (w - kw) // sw + 1
     else:
         ph, pw = padding
+        oh = (h + 2 * ph - kh) // sh + 1
+        ow = (w + 2 * pw - kw) // sw + 1
     padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)))
-    oh = (h + 2 * ph - kh) // sh + 1
-    ow = (w + 2 * pw - kw) // sw + 1
     output = np.zeros((m, oh, ow))
     for i in range(oh):
         for j in range(ow):